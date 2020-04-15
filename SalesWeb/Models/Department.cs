78d03e272dc73e8134382c63d32630a8378bacc8
jose Duque <joseduquefilho03@gmail.com>
2020-04-15 15:07:17 -0300

    Criação de Entidades

--- SalesWeb/Models/Department.cs
+++ SalesWeb/Models/Department.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System;
 using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace SalesWeb.Models
 {
@@ -9,5 +9,25 @@ namespace SalesWeb.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department()
+        {
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+        public double TotalSales(DateTime inicial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(inicial, final));
+        }
     }
+
 }
